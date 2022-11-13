5a95bfd141574c81b61419f75760d930037e143f
Nicolas1302 <nicoebon@gmail.com>
2022-11-13 12:27:58 -0300

    paula

---
+++ proyectogrupal/src/main/java/com/proyecto/empleado.java
@@ -0,0 +1,38 @@
+package com.proyecto;
+
+public class empleado {
+    private String nombre;
+    private String apellido;
+    private String fecha_nacimiento;
+
+    public empleado(){
+        this.nombre = "Thiago";
+        this.apellido = "Moreno";
+        this.fecha_nacimiento = "1984-04-14";
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+    
+}
