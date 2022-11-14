53b13a863cd7e3c70299a834ead9e512ac47957a
Nicolas1302 <nicoebon@gmail.com>
2022-11-14 20:20:22 -0300

    actualizacion

---
+++ proyecto_grupal/main/src/main/java/com/proyecto_grupal/App.java
@@ -0,0 +1,13 @@
+package com.proyecto_grupal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
