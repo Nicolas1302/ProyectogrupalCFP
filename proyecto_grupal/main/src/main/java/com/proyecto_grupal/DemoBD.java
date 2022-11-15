d3f1b85cb137fa54020955add36860f813ab11a6
aleexsb <universal.inc@hotmail.com>
2022-11-14 21:42:56 -0300

    correcion

--- proyecto_grupal/main/src/main/java/com/proyecto_grupal/DemoBD.java
+++ proyecto_grupal/main/src/main/java/com/proyecto_grupal/DemoBD.java
@@ -2,10 +2,12 @@ package com.proyecto_grupal;
 
 public class DemoBD {
     public static void main( String[] args ){
-        
+
         empleadoDTO Empleado = new empleadoDTO();
 
-        //Empleado.saveEmpleado("adrian","ferre","2022-04-12",1);
+        Empleado.saveSueldo(10000,150000);
+        Empleado.getSueldo();
+        Empleado.saveEmpleado("Juan", "Domínguez", "1992-02-05", 2);
         Empleado.getEmpleado();
     }
 }
