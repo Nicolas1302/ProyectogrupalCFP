1009f2db21a0328928abedd48fe4f6b0c9c64023
LNPoly <paula.domignuez2205@gmail.com>
2022-11-25 23:00:50 -0300

    Actualizacion empleado -Controller/Builder-

--- proyecto_grupal/main/src/main/java/com/proyecto_grupal/EmpleadoController.java
+++ proyecto_grupal/main/src/main/java/com/proyecto_grupal/EmpleadoController.java
@@ -1,38 +1,47 @@
 package com.proyecto_grupal;
 
-
-
 import java.io.FileReader;
-
-
+import java.util.ArrayList;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-
-
-
 public class EmpleadoController {
     public static void main( String[] args ){
-
-       
+        
         empleadoService empleadoService = new empleadoService();
+        ArrayList<empleado> empleadoBase = empleadoService.getEmpleados();
+
+        int x = 0;
+        while(x < empleadoBase.size() - 1){ 
+            JSONObject Empleado = new JSONObject();
+            Empleado.put("Nombre", empleadoBase.get(x).getNombre());
+            Empleado.put("Apellido", empleadoBase.get(x).getApellido());
+            Empleado.put("FechaNacimiento",empleadoBase.get(x).getFecha_nacimiento());
+            Empleado.put(x,Empleado);
+            x++;
+        }
 
         JSONParser jsonParser = new JSONParser();
         empleadoBuilder empleadoBuilder = new empleadoBuilder();
 
         try(FileReader reader = new FileReader("empleados.json")){
+            ArrayList<empleado> empleados = new ArrayList<empleado>();
             Object obj = jsonParser.parse(reader);
             JSONArray jsonEmpleados = (JSONArray) obj;
-            empleado empleado = empleadoBuilder.builderEmpleado((JSONObject)jsonEmpleados.get(0));
-            System.out.println(empleado);
-            
-            
-            empleadoService.validateAndSaveEmpleado(empleado);
-
+            for (x = 0; x < jsonEmpleados.size(); x++){
+                empleado empleado1 = empleadoBuilder.builderEmpleado((JSONObject)jsonEmpleados.get(x),"" + x);
+                System.out.println(empleado1);
+                System.out.println(x);
+                empleados.add(empleado1);
+                }
+    
+                int indice = 0;
+            while(indice < empleados.size()){
+            empleadoService.validateAndSaveEmpleado(empleados.get(indice));
+            indice++;
+            }
         } catch (Exception e){
             e.printStackTrace();
         }
     }
-    
 }
-
