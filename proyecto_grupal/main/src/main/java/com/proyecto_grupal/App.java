99699a1e541169bb4c9c69c43d46637fc4233e7e
Nicolas1302 <nicoebon@gmail.com>
2022-12-13 12:51:34 -0300

    ACTUALIZACION FINAL

--- proyecto_grupal/main/src/main/java/com/proyecto_grupal/App.java
+++ proyecto_grupal/main/src/main/java/com/proyecto_grupal/App.java
@@ -10,10 +10,10 @@ public class App {
         jornadaController JornadaController = new jornadaController();
         EmpleadoController EmpleadoController = new EmpleadoController();
 
-        //EmpleadoController.getEmpleado();//trae los Empleados de la Base de Datos en JSON empleadosBase.json
-        //EmpleadoController.postEmpleado();//toma los Datos de Empleado.json y lo carga a la Base de Datos
-        //JornadaController.getjornada();//Trae los datos de Jornada laboral de la Base De Datos en JSON jornadaBase.json
-        //JornadaController.postJornada();//toma los Datos de jornada.json y lo carga a la Base de Datos en la tabla JornadaLaboral
+        EmpleadoController.getEmpleado();//trae los Empleados de la Base de Datos en JSON empleadosBase.json
+        EmpleadoController.postEmpleado();//toma los Datos de Empleado.json y lo carga a la Base de Datos
+        JornadaController.getjornada();//Trae los datos de Jornada laboral de la Base De Datos en JSON jornadaBase.json
+        JornadaController.postJornada();//toma los Datos de jornada.json y lo carga a la Base de Datos en la tabla JornadaLaboral
         JornadaController.getSueldos();//Trae los Calculos de los SueldosBruto y Neto dependiendo de los Horarios de Entrada y Salida en JSON SueldoBase.json
     }
 }
